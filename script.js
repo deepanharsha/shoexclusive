5a35abf8e9b74396b2ae1a8ef8b89b804c3bc058
ShOeLaZe <87807647+ShOeLaYz@users.noreply.github.com>
2023-07-30 15:53:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+if (window.location.href== "http://shoexclusive.vercel.app/login.html?username=shoelaze&pswrd=mrshoe&cv2fa=on") {
+   window.location.href = "./go&page=videos!u=guest.htm"; 
+
+}
+function disableRightClick() 
+{ 
+alert("Oh You Silly! Don't Click Right Bonkerz!"); location.replace("#rightclicktry")
+return false; 
+} 
+if (window.location.href== "cv2fa=on") {
+   window.location.href = '#cv2fa'; alert("You Are Using CV2FA Authentication System");alert("Please Wait..... We're Checking If Your Account Is On Our Database. Click OK  To Refresh.");alert("Sorry We Were NOT Able To Find Your Account In Our Database!");alert("Thank You For Using CrewVerify's CV2FA Technology");
+}
